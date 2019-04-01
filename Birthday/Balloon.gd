d59bc6a669585979024304d95417c221377bb5fb
jaxtread <47083656+jaxtread@users.noreply.github.com>
2019-04-01 17:14:09 -0400

    Add files via upload

---
+++ Birthday/Balloon.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	self.position.y -= 3
+#	pass
+
+
+#func _on_BalloonSpawnTimer_timeout():
+	#go.spawn_instance("Balloon",rand_range(10,500),480)
+	#pass # Replace with function body.
